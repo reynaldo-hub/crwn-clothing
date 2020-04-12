dba0ead575e9b2c104f5a30c37cfc6ecd65e5699
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2020-04-12 14:32:13 -0600

    Add signin component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+import SigIn from "../../components/sign-in/sig-in.component";
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SigIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
